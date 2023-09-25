bc3cb3169e488815ae22273b01ad5b44920cd833
Daavp <118278693+Daavp@users.noreply.github.com>
2023-09-25 00:53:10 -0300

    commit

--- src/public/js/payments.js
+++ src/public/js/payments.js
@@ -3,10 +3,9 @@ console.log("payments js");
 const button = document.getElementById('checkout');
 button.addEventListener('click', async()=>{
 const res = await fetch(`/api/payments/create-checkout-session`, {
-    method: 'POST',mode:"cors"
+    method: 'POST',
 });
 const data = await res.json();
 console.log(data);
 window.location.href = data.url
-
 });
