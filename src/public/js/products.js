a499cdecf23a6f5e99d78cc8ee6779bbe7dde59b
Daavp <118278693+Daavp@users.noreply.github.com>
2023-09-24 23:45:06 -0300

    commit

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -14,7 +14,8 @@ console.log("js products");
 /*         result = await resp.json();
         console.log("Status: ",result.status,"Message: ",result.message); */
     } catch (error) {
-        console.log("No hay sesión iniciada", error.message)
+        console.log("No hay sesión iniciada")
+        console.log(error.message)
     }
     }
 //fetch de datos para crear carrito o agregar info a carrito
